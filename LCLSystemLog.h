3d9789be4c5eaf87d06cb887e8962edee8ddfa10
Arne Harren <ah@0xc0.de>
2010-01-02 22:13:54 +0100

    initial commit

---
+++ LCLSystemLog.h
@@ -0,0 +1,109 @@
+//
+//
+// LCLSystemLog.h
+//
+//
+// Copyright (c) 2008-2009 Arne Harren <ah@0xc0.de>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#define _LCLSYSTEMLOG_VERSION_MAJOR  1
+#define _LCLSYSTEMLOG_VERSION_MINOR  0
+#define _LCLSYSTEMLOG_VERSION_BUILD  2
+
+//
+// LCLSystemLog
+//
+// LCLSystemLog is a LibComponentLogging logger implementation which sends
+// log messages to the Apple System Log facility (ASL). See asl, syslog, and
+// syslogd for details.
+//
+// With ASL, log messages are stored as structured messages in a data store.
+// The syslog utility or the Console application can be used to retrieve
+// messages from this data store, e.g.
+//
+//   syslog -F '$(Time) $(Sender)[$(PID):$(Thread)] $(Level0) $(Message) ($(Facility):$(File):$(Line))' -T utc -k Level0 -k Sender eq Example
+//
+// retrieves all messages from the data store where the value associated with
+// the 'Sender' key (the identifier of an application) is equal to 'Example' and
+// where a value for the 'Level0' key exists. The key 'Level0' is used by
+// LCLSystemLog to store the LCL log level in addition to a mapped ASL priority
+// level ('Level' key). All retrieved messages will be printed by using the UTC
+// time format and the display format specified via -F. Example output:
+//
+//   2009.02.21 11:37:08 UTC Example[6717:10b] D Message 1 (example.f1:main.m:28)
+//   2009.02.21 11:37:08 UTC Example[6717:10b] C Message 2 (example.f2:main.m:32)
+//   2009.02.21 11:37:08 UTC Example[6717:10b] I Message 3 (example.f3:main.m:36)
+//
+// By default, the default data store will only save log messages which have
+// an ASL priority level between 'Emergency' and 'Notice' (level 0 to 5). Log
+// messages with level 'Info' (level 6) or 'Debug' (level 7) will not be written
+// to the data store. The command line
+//
+//   sudo syslog -c syslog -d
+//
+// can be used to tell syslogd to store messages up to priority level 'Debug'.
+//
+//
+// LCLSystemLog is configured via the following define which should be specified
+// in lcl_config_logger.h:
+//
+// - Mirror log messages to stderr? (type BOOL)
+//   #define _LCLSystemLog_MirrorMessagesToStdErr <definition>
+//
+//
+// Note: When using LCLSystemLog, please use a 'Reverse ICANN' naming scheme
+//       for log component headers as suggested by the ASL documentation.
+//
+
+
+//
+// LCLSystemLog class.
+//
+
+
+@interface LCLSystemLog : NSObject {
+    
+}
+
+// Writes the given log message to the system log.
++ (void)writeComponent:(_lcl_component_t)component level:(_lcl_level_t)level
+                  path:(const char *)path line:(uint32_t)line
+               message:(NSString *)message, ... __attribute__((format(__NSString__, 5, 6)));
+
+@end
+
+
+//
+// Integration with LibComponentLogging Core.
+//
+
+
+// Definition of _lcl_logger.
+#define _lcl_logger(log_component, log_level, log_format, ...) {               \
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];                \
+    [LCLSystemLog writeComponent:log_component                                 \
+                           level:log_level                                     \
+                            path:__FILE__                                      \
+                            line:__LINE__                                      \
+                         message:log_format,                                   \
+                              ## __VA_ARGS__];                                 \
+    [pool release];                                                            \
+}
+
---
+++ LCLSystemLog.m
@@ -0,0 +1,138 @@
+//
+//
+// LCLSystemLog.m
+//
+//
+// Copyright (c) 2008-2009 Arne Harren <ah@0xc0.de>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "lcl.h"
+
+#ifndef _LCLSystemLog_MirrorMessagesToStdErr
+#error  '_LCLSystemLog_MirrorMessagesToStdErr' must be defined in 'lcl_config_logger.h'
+#endif
+
+#include <asl.h>
+#include <mach/mach_init.h>
+
+
+@interface LCLSystemLog (Internals)
+
+// A lock which is held when the asl client connection is used.
+static NSRecursiveLock *_LCLSystemLog_aslClientLock = nil;
+
+// The asl client connection to use.
+static aslclient _LCLSystemLog_aslClient = NULL;
+
+// Log levels known by asl, indexed by LCL log level.
+static const char * const _LCLSystemLog_aslLogLevel[] = {
+    ASL_STRING_DEBUG,   // Off
+    ASL_STRING_CRIT,    // Critical
+    ASL_STRING_ERR,     // Error
+    ASL_STRING_WARNING, // Warning
+    ASL_STRING_NOTICE,  // Info
+    ASL_STRING_DEBUG,   // Debug
+    ASL_STRING_DEBUG    // Trace
+};
+
+@end
+
+
+@implementation LCLSystemLog
+
+// No instances, please.
++(id)alloc {
+    [LCLSystemLog doesNotRecognizeSelector:_cmd];
+    return nil;
+}
+
+// Initializes the class.
++ (void)initialize {
+    // perform initialization only once
+    if (self != [LCLSystemLog class])
+        return;
+    
+    // create the lock
+    _LCLSystemLog_aslClientLock = [[NSRecursiveLock alloc] init];
+    
+    // open an asl client connection
+    uint32_t client_opts = 0;
+    if (_LCLSystemLog_MirrorMessagesToStdErr) {
+        // mirror log messages to stderr if requested
+        client_opts |= ASL_OPT_STDERR;
+    }
+    _LCLSystemLog_aslClient = asl_open(NULL, NULL, client_opts);
+    
+    // log all messages up to DEBUG
+    if (_LCLSystemLog_aslClient != NULL) {
+        asl_set_filter(_LCLSystemLog_aslClient, ASL_FILTER_MASK_UPTO(ASL_LEVEL_DEBUG));
+    }
+}
+
+// Writes the given log message to the system log.
++ (void)writeComponent:(_lcl_component_t)component level:(_lcl_level_t)level
+                  path:(const char *)path line:(uint32_t)line
+               message:(NSString *)message, ... {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    // get file from path
+    const char *file_c = strrchr(path, '/');
+    file_c = (file_c != NULL) ? (file_c + 1) : (path);
+    
+    // get line number
+    char line_c[12];
+    snprintf(line_c, sizeof(line_c), "%d", line);
+    
+    // get thread id
+    char tid_c[10];
+    snprintf(tid_c, sizeof(tid_c), "%x", mach_thread_self());
+    
+    // create log message
+    va_list args;
+    va_start(args, message);
+    NSString *msg = [[[NSString alloc] initWithFormat:message arguments:args] autorelease];
+    va_end(args);
+    const char *msg_c = [msg UTF8String];
+    
+    // create the system log message
+    aslmsg msg_asl = asl_new(ASL_TYPE_MSG);
+    asl_set(msg_asl, ASL_KEY_FACILITY, _lcl_component_header[component]);
+    asl_set(msg_asl, ASL_KEY_LEVEL, _LCLSystemLog_aslLogLevel[level]);
+    asl_set(msg_asl, "Level0", _lcl_level_header_1[level]);
+    asl_set(msg_asl, ASL_KEY_MSG, msg_c);
+    asl_set(msg_asl, "Thread", tid_c);
+    asl_set(msg_asl, "File", file_c);
+    asl_set(msg_asl, "Line", line_c);
+    
+    // send the system log message
+    [_LCLSystemLog_aslClientLock lock];
+    {
+        asl_send(_LCLSystemLog_aslClient, msg_asl);
+    }
+    [_LCLSystemLog_aslClientLock unlock];
+    
+    // free the system log message
+    asl_free(msg_asl);
+    
+    [pool release];
+}
+
+@end
+
